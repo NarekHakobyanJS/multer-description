a6eb5ed0655177956c6a95acd99547dd78ece4e0
NarekHakobyanJS <haobyan95narek@gmail.com>
2024-10-26 18:18:42 +0400

    use ui

--- app.js
+++ app.js
@@ -29,7 +29,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'uploads')))
+
 
 app.use('/', indexRouter);
 
